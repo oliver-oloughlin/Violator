01a0a11e14184c3c31f16af03a265e67561d268d
oliver-oloughlin <oliveroloughlin@hotmail.com>
2022-10-21 14:11:55 +0200

    FIrst try reactions

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
-import { createBot, startBot, Intents } from "https://deno.land/x/discordeno@17.0.0/mod.ts"
+import { createBot, startBot, Intents, addReaction } from "https://deno.land/x/discordeno@17.0.0/mod.ts"
 import { send } from "./utils/message.ts"
 import "https://deno.land/std@0.160.0/dotenv/load.ts"
+import { CHANNEL_ID, LORD_ID } from "./utils/constants.ts"
 
 const token = Deno.env.get("DISCORD_TOKEN")
 if (!token) throw Error("Token not initilized")
@@ -12,6 +13,7 @@ const bot = createBot({
     ready: () => console.log("Successfully connected to gateway"),
     messageCreate: (bot, msg) => {
       if (msg.isFromBot) return
+      if (msg.authorId === LORD_ID) addReaction(bot, CHANNEL_ID, msg.id, ":eirik:")
       setTimeout(() => {
         const member = msg.member!
         send(bot, member)
